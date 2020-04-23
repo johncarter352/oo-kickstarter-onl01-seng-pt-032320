19291a6420b90f67f92a7727942968e42cc1892d
John Carter <johncarter352@gmail.com>
2020-04-23 06:24:52 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,6 +6,7 @@ class Project
     @backers = []
   end
   
-  def add_backer(backer = @backer)
-    @backers
+  def add_backer(backer)
+    @backers << backer 
+    backer.backed_projects << self
 end
